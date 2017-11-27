e6edc7199db8e21a6caca6d28490fd382ce409ae
cedjud <cedricjud@outlook.com>
2017-11-27 20:27:07 +0100

    :ambulance: Append card correctly to carousel

--- newcard.js
+++ newcard.js
@@ -1,67 +1,78 @@
 
 var form = document.getElementById('addForm');
 
-var itemList = document.getElementById('items'); 
+/**
+ * get the correct element to append the new carousel item to
+ */
+// var itemList = document.getElementById('items');
+var itemList = document.querySelector('.carousel-inner');
+
 
 // form submit event
 
 form.addEventListener('submit', addItem);  // create another functions, to clear form and close wclose module winddow.)
 
-// form delete event 
+// form delete event
 
 itemList.addEventListener('click', removeItem);
 
-// add item  fun fun Function 
+
+// add item  fun fun Function
 
 function addItem(e){
-  
+
   e.preventDefault();
-  
+
   // get input value in the modal
-  
+
   var newItem = document.getElementById('item').value;
-  
+
   var newItemTwo = document.getElementById('time').value;
-  
+
   var newItemThree = document.getElementById('name-of-client').value;
-  
+
   var newItemFour = document.getElementById('phone-number-client').value;
 
   var newItemFive = document.getElementById('acces-notes').value;
 
-  
+
   // create a new li element with the text grab from above
-  
+
   var li = document.createElement('li');
-      li.className = 'list-group-item';
+      /**
+       * Set the correct class name for the carousel to work
+       */
+      // li.className = 'list-group-item';
+      li.className = 'item';
+
       li.style.height = '150px';
       console.log(li)
-  
+
   // create a new p element with the text grab from above
-      
+
   var pOne  = document.createElement('p');
       pOne.className = 'items';
-        
+
   var pTwo  = document.createElement('p');
       pOne.className = 'items';
-      console.log(pTwo)    
-          
+      console.log(pTwo)
+
   var pThree  = document.createElement('p');
       pOne.className = 'items';
-      console.log(pThree)  
-      
+      console.log(pThree)
+
   var pFour  = document.createElement('p');
       pOne.className = 'items';
-      console.log(pFour)         
-  
+      console.log(pFour)
+
   // add text node with input value
-  
+
   li.appendChild(document.createTextNode(newItem));
-  
+
   pOne.appendChild(document.createTextNode(newItemTwo));
-  
+
   pTwo.appendChild(document.createTextNode(newItemThree));
-  
+
   pThree.appendChild(document.createTextNode(newItemFour));
 
   pFour.appendChild(document.createTextNode(newItemFive));
@@ -72,65 +83,69 @@ function addItem(e){
       deleteBtn.style.width = '80px';
       deleteBtn.style.float = 'right';
       deleteBtn.style.height = '100%';
-  
+
   // add the classes to the delete BUTTON
-  
+
   deleteBtn.className = "btn btn-danger btn-sm float-right delete";
-  
-  
-  
-  
-  // append the text node 
-  
+
+
+
+
+  // append the text node
+
   deleteBtn.appendChild(document.createTextNode('X'));
-  
+
   // append delete button into the LI
-  
+
   li.appendChild(deleteBtn);
-  
-  // append li to list. 
-  
-  itemList.appendChild(li);
-  
-  // append p elements into LI; (not sure if this is the best way but its working)  
-  li.appendChild(pOne); 
-  li.appendChild(pTwo); 
+
+  // append li to list.
+
+  /**
+   * insert the new element at the beginning of the itemList element, not at the end
+   */
+  // itemList.appendChild(li);
+  itemList.insertBefore(li, itemList.firstChild);
+
+  // append p elements into LI; (not sure if this is the best way but its working)
+  li.appendChild(pOne);
+  li.appendChild(pTwo);
   li.appendChild(pThree);
   li.appendChild(pFour);
-  li.appendChild(pFive); 
-  
-   
-  
-  
+  li.appendChild(pFive);
+
+
+
+
 }
 
 
 
 
 
-// create a function to clear the form 
-
-  
-
-// create a function to cloes the modal window. 
+// create a function to clear the form
 
 
 
-// Remove Item Function 
+// create a function to cloes the modal window.
+
+
+
+// Remove Item Function
 
 function removeItem(e){
-  
+
   if (e.target.classList.contains('delete')){
-    
+
     if (confirm('Are you sure bro?')){
-      
+
       var li = e.target.parentElement;
       itemList.removeChild(li);
-      
+
     }
-    
+
   }
-  
+
 }
 
 
@@ -141,7 +156,7 @@ function removeItem(e){
 
 
 
-// add item 
+// add item
 
 
 
@@ -157,16 +172,16 @@ function removeItem(e){
 
 // var div = document.createElement('div');
 // div.className = 'form-group';
-// 
+//
 // var label = document.createElement('label');
 // label.className = 'col-sm-2 control-label';
 // label.innerHTML = 'Whoa this guy you!!!!';
 // label.for = 'inputText';
-// 
+//
 // var div1 = document.createElement('div');
 // div1.className = 'col-sm-10';
-// 
-// 
+//
+//
 // var commentText = document.createElement('textarea');
 // commentText.className = 'form-control';
 // commentText.id = 'inputText';
@@ -175,5 +190,5 @@ function removeItem(e){
 // div.appendChild(label);
 // div1.appendChild(commentText);
 // div.appendChild(div1);
-// 
+//
 // document.body.appendChild(div);
