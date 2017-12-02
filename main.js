a755aed688b860f39cc8e095997c3b187885f447
Jonathan Aqauarone <J.aquarone@gmail.com>
2017-12-02 09:26:20 -0500

    omg omg omg got the local storage work

--- main.js
+++ main.js
@@ -1,3 +1,76 @@
-// $('#myModal').on('shown.bs.modal', function () {
-//   $('#myInput').focus()
-// })
+document.getElementById('issueInputForm').addEventListener('submit', saveIssue);
+
+function saveIssue(e) {
+  
+  
+  var issueAddy = document.getElementById('appointmentAddress').value;
+  var issueDesc = document.getElementById('issueDescInput').value;
+  var issueAssignedTo = document.getElementById('issueAssignedToInput').value;
+  var issueId = chance.guid();
+  var issueStatus = 'Open';
+
+  var issue = {
+    address: issueAddy, 
+    id: issueId,
+    description: issueDesc,  
+    assignedTo: issueAssignedTo,
+    status: issueStatus
+  }
+
+  if (localStorage.getItem('issues') == null) {
+    var issues = [];
+    issues.push(issue);
+    localStorage.setItem('issues', JSON.stringify(issues));
+  } else {
+    var issues = JSON.parse(localStorage.getItem('issues'));
+    issues.push(issue);
+    localStorage.setItem('issues', JSON.stringify(issues));
+  }
+
+  document.getElementById('issueInputForm').reset();
+
+  fetchIssues();
+
+  e.preventDefault();
+}
+
+
+
+function deleteIssue(id) {
+  var issues = JSON.parse(localStorage.getItem('issues'));
+  for (var i = 0; i < issues.length; i++) {
+    if (issues[i].id == id) {
+      issues.splice(i, 1);
+    }
+  }
+
+  localStorage.setItem('issues', JSON.stringify(issues));
+
+  fetchIssues();
+}
+
+function fetchIssues() {
+  var issues = JSON.parse(localStorage.getItem('issues'));
+  var issuesListe = document.getElementById('issuesList');
+
+  issuesList.innerHTML = '';
+
+  for (var i = 0; i < issues.length; i++) {
+    
+    
+    var address = issues[i].address;
+    var id = issues[i].id;
+    var desc = issues[i].description;
+    var assignedTo = issues[i].assignedTo;
+    var status = issues[i].status;
+
+    issuesList.innerHTML +=   '<div class="well">'+
+                              '<h3>' + address + '</h3>'+
+                              '<h3>' + desc + '</h3>'+
+                              '<h3>' + assignedTo + '</h3>'+
+                              '<a href="#" onclick="deleteIssue(\''+id+'\')" class="btn btn-danger">Delete</a>'+
+                              '</div>';
+                                                          
+  }
+}
+
--- newcard.js
+++ newcard.js
@@ -1,220 +1,200 @@
 
-var form = document.getElementById('addForm');
-
-/**
- * get the correct element to append the new carousel item to
- */
-// var itemList = document.getElementById('items');
-var itemList = document.querySelector('.carousel-inner');
-
-var saveCardInfo = [];
-
-// form submit event
-
-form.addEventListener('submit', addItem);  
-
-// form delete event
-
-itemList.addEventListener('click', removeItem);
+// I created a whole new js file so check main.js
 
 
-const items = JSON.parse(localStorage.getItem('saveCardInfo')) || [];
-
-
-
-
-// add item  fun fun Function
-
-function addItem(e){
-
-  e.preventDefault();
-
-  // get input value from the modal
-
-  var newItem = document.getElementById('item').value;
-  
-  var newItemTwo = document.getElementById('time').value;
-
-  var newItemThree = document.getElementById('name-of-client').value;
-
-  var newItemFour = document.getElementById('phone-number-client').value;
-  
-  var newItemFive = document.getElementById('acces-notes').value;
-  
-  // create an object and place all aboove input value in it. 
-
-  var formObject = {
-    
-      item: newItem,
-      time: newItemTwo,
-      nameOfClient:newItemThree,
-      phoneNumberClient:newItemFour,
-      accessNotes:newItemFive  
-  }
-
-  saveCardInfo.push(formObject);
-
-  localStorage.setItem('saveCardInfo', JSON.stringify(saveCardInfo));
-  
-  // create a new li element with the text grab from above
-
-
-
-  var li = document.createElement('li');
-      /**
-       * Set the correct class name for the carousel to work
-       */
-      // li.className = 'list-group-item';
-      li.className = 'item';
-      li.style.maxWidth = '95%';
-      li.style.border = 'solid 1px #d3d3d3';
-      li.style.padding = '10px';
-      li.style.height = '150px';
-      li.style.marginRight = '12%';
-      li.style.marginLeft = '12%';
-      li.style.fontWeight = '500';
-      li.style.fontSize = '14px';
-      
-
-  // create a new p element with the text grab from above
-
-  var pOne  = document.createElement('p');
-      pOne.className = 'items';
-      pOne.style.fontSize = '12px';
-      pOne.style.fontWeight = '400';
-      pOne.style.marginTop = '5px';
-      
-
-  var pTwo  = document.createElement('p');
-      pTwo.className = 'items';
-      pTwo.style.fontSize = '12px';
-      pTwo.style.fontWeight = '200';
-      
-    
-
-  var pThree  = document.createElement('p');
-      pThree.className = 'items';
-      pThree.style.fontSize = '12px';
-      pThree.style.fontWeight = '200';
-    
-
-  var pFour  = document.createElement('p');
-      pFour.className = 'items';
-      pFour.style.fontSize = '12px';
-      pFour.style.fontWeight = '200';
-      
-
-  // add text node with input value
-
-  li.appendChild(document.createTextNode(newItem));
-
-  pOne.appendChild(document.createTextNode(newItemTwo));
-
-  pTwo.appendChild(document.createTextNode(newItemThree));
-
-  pThree.appendChild(document.createTextNode(newItemFour));
-
-  pFour.appendChild(document.createTextNode(newItemFive));
-
-
-  // create delete BUTTON
-  var deleteBtn =  document.createElement('button');
-      deleteBtn.style.width = '50px';
-      deleteBtn.style.float = 'right';
-      deleteBtn.style.height = '50px';
-      deleteBtn.style.borderRadius = '50%'
-      deleteBtn.style.border = 'solid 1px red';
-      deleteBtn.style.backgroundColor = 'white';
-      deleteBtn.style.color = 'red';
-      
-
-  // add the classes to the delete BUTTON
-
-  deleteBtn.className = "btn btn-danger btn-sm float-right delete";
-
-  // append the text node
-
-  deleteBtn.appendChild(document.createTextNode('X'));
-
-  // append delete button into the LI
-
-  li.appendChild(deleteBtn);
-
-  // append li to list.
-
-  /**
-   * insert the new element at the beginning of the itemList element, not at the end
-   */
-  // itemList.appendChild(li);
-  itemList.insertBefore(li, itemList.firstChild);
-
-  // append p elements into LI; (not sure if this is the best way but its working)
-  li.appendChild(pOne);
-  li.appendChild(pTwo);
-  li.appendChild(pThree);
-  li.appendChild(pFour);
-  
-  
-  // localStorage.setItems('saveCardInfo', JSON.stringify(saveCardInfo));
-
-  this.reset();
-  
-   // .modal('hide'); might have to store the info first
-
-    // }
-  
-}
-
-// function myCloseModalFunction () {
+// var form = document.getElementById('addForm');
 // 
-//   var x = document.getElementById('exampleModalLong');
+// /**
+//  * get the correct element to append the new carousel item to
+//  */
+// // var itemList = document.getElementById('items');
+// var itemList = document.querySelector('.carousel-inner');
 // 
-//   if (x.style.visibility === "visible") {
+// var saveCardInfo = [];
 // 
-//     x.style.visibility = 'hidden';
+// // form submit event
 // 
-//   } else {
+// form.addEventListener('submit', addItem);  
 // 
-//     x.style.display = "none";
+// // form delete event
+// 
+// itemList.addEventListener('click', removeItem);
+// 
+// 
+// const items = JSON.parse(localStorage.getItem('saveCardInfo')) || [];
+// 
+// 
+// 
+// 
+// // add item  fun fun Function
+// 
+// function addItem(e){
+// 
+//   e.preventDefault();
+// 
+//   // get input value from the modal
+// 
+//   var newItem = document.getElementById('item').value;
+// 
+//   var newItemTwo = document.getElementById('time').value;
+// 
+//   var newItemThree = document.getElementById('name-of-client').value;
+// 
+//   var newItemFour = document.getElementById('phone-number-client').value;
+// 
+//   var newItemFive = document.getElementById('acces-notes').value;
+// 
+//   // create an object and place all aboove input value in it. 
+// 
+//   var formObject = {
+// 
+//       item: newItem,
+//       time: newItemTwo,
+//       nameOfClient:newItemThree,
+//       phoneNumberClient:newItemFour,
+//       accessNotes:newItemFive  
+//   }
+// 
+//   saveCardInfo.push(formObject);
+// 
+//   localStorage.setItem('saveCardInfo', JSON.stringify(saveCardInfo));
+// 
+//   // create a new li element with the text grab from above
+// 
+// 
+// 
+//   var li = document.createElement('li');
+//       /**
+//        * Set the correct class name for the carousel to work
+//        */
+//       // li.className = 'list-group-item';
+//       li.className = 'item';
+//       li.style.maxWidth = '95%';
+//       li.style.border = 'solid 1px #d3d3d3';
+//       li.style.padding = '10px';
+//       li.style.height = '150px';
+//       li.style.marginRight = '12%';
+//       li.style.marginLeft = '12%';
+//       li.style.fontWeight = '500';
+//       li.style.fontSize = '14px';
+// 
+// 
+//   // create a new p element with the text grab from above
+// 
+//   var pOne  = document.createElement('p');
+//       pOne.className = 'items';
+//       pOne.style.fontSize = '12px';
+//       pOne.style.fontWeight = '400';
+//       pOne.style.marginTop = '5px';
+// 
+// 
+//   var pTwo  = document.createElement('p');
+//       pTwo.className = 'items';
+//       pTwo.style.fontSize = '12px';
+//       pTwo.style.fontWeight = '200';
+// 
+// 
+// 
+//   var pThree  = document.createElement('p');
+//       pThree.className = 'items';
+//       pThree.style.fontSize = '12px';
+//       pThree.style.fontWeight = '200';
+// 
+// 
+//   var pFour  = document.createElement('p');
+//       pFour.className = 'items';
+//       pFour.style.fontSize = '12px';
+//       pFour.style.fontWeight = '200';
+// 
+// 
+//   // add text node with input value
+// 
+//   li.appendChild(document.createTextNode(newItem));
+// 
+//   pOne.appendChild(document.createTextNode(newItemTwo));
+// 
+//   pTwo.appendChild(document.createTextNode(newItemThree));
+// 
+//   pThree.appendChild(document.createTextNode(newItemFour));
+// 
+//   pFour.appendChild(document.createTextNode(newItemFive));
+// 
+// 
+//   // create delete BUTTON
+//   var deleteBtn =  document.createElement('button');
+//       deleteBtn.style.width = '50px';
+//       deleteBtn.style.float = 'right';
+//       deleteBtn.style.height = '50px';
+//       deleteBtn.style.borderRadius = '50%'
+//       deleteBtn.style.border = 'solid 1px red';
+//       deleteBtn.style.backgroundColor = 'white';
+//       deleteBtn.style.color = 'red';
+// 
+// 
+//   // add the classes to the delete BUTTON
+// 
+//   deleteBtn.className = "btn btn-danger btn-sm float-right delete";
+// 
+//   // append the text node
+// 
+//   deleteBtn.appendChild(document.createTextNode('X'));
+// 
+//   // append delete button into the LI
+// 
+//   li.appendChild(deleteBtn);
+// 
+//   // append li to list.
+// 
+//   /**
+//    * insert the new element at the beginning of the itemList element, not at the end
+//    */
+//   // itemList.appendChild(li);
+//   itemList.insertBefore(li, itemList.firstChild);
+// 
+//   // append p elements into LI; (not sure if this is the best way but its working)
+//   li.appendChild(pOne);
+//   li.appendChild(pTwo);
+//   li.appendChild(pThree);
+//   li.appendChild(pFour);
+// 
+// 
+//   // localStorage.setItems('saveCardInfo', JSON.stringify(saveCardInfo));
+// 
+//   this.reset();
+// 
+//    // .modal('hide'); might have to store the info first
+// 
+//     // }
+// 
+// }
+// 
+// 
+// 
+// 
+// 
+// // Remove Item Function
+// 
+// var removeItem = function(e){
+// 
+//     console.log(e.target)
+// 
+// 
+//   if (e.target.classList.contains('delete')){
+// 
+//     if (confirm('Are you sure catMan?')){
+// 
+//       var li = document.querySelector('.item.active');
+// 
+//       itemList.removeChild(li);
+// 
+//       document.querySelector('.item').classList.add('active')
+// 
+//     }
 // 
 //   }
 // 
-// 
 // }
-
-
-
-
-
-
-
-// create a function to cloes the modal window.
-
-
-
-// Remove Item Function
-
-function removeItem(e){
-
-    console.log(e.target)
-  
-
-  if (e.target.classList.contains('delete')){
-
-    if (confirm('Are you sure bro?')){
-
-      var li = document.querySelector('.item.active');
-      
-      itemList.removeChild(li);
-      
-      document.querySelector('.item').classList.add('active')
-
-    }
-
-  }
-
-}
-
+// 
 
 
 // <-----------------------module section--------->
